6de4692fdb404321ca65aafa9476af1adc0b3ee8
nlom0218 <nlom0218@naver.com>
2023-09-09 15:00:53 +0900

    chore: 초기 설정

---
+++ src/apis/gifAPIService.ts
@@ -0,0 +1,59 @@
+import { GifsResult, GiphyFetch, SearchOptions } from '@giphy/js-fetch-api';
+import { IGif } from '@giphy/js-types';
+
+import { GifImageModel } from '../models/image/gifImage';
+
+const apiKey = process.env.GIPHY_API_KEY || '';
+const gf = new GiphyFetch(apiKey);
+
+const DEFAULT_FETCH_COUNT = 16;
+const TRENDING_GIF_API = `https://api.giphy.com/v1/gifs/trending?api_key=${process.env.GIPHY_API_KEY}&limit=${DEFAULT_FETCH_COUNT}&rating=g`;
+
+function convertResponseToModel(gifList: IGif[]): GifImageModel[] {
+  return gifList.map((gif) => {
+    const { id, title, images } = gif;
+
+    return {
+      id,
+      title,
+      imageUrl: images.original.url
+    };
+  });
+}
+
+export const gifAPIService = {
+  /**
+   * treding gif 목록을 가져옵니다.
+   * @returns {Promise<GifImageModel[]>}
+   * @ref https://developers.giphy.com/docs/api/endpoint#!/gifs/trending
+   */
+  getTrending: async function (): Promise<GifImageModel[]> {
+    try {
+      const gifs: GifsResult = await fetch(TRENDING_GIF_API).then((res) => res.json());
+      return convertResponseToModel(gifs.data);
+    } catch (e) {
+      return [];
+    }
+  },
+  /**
+   * 검색어에 맞는 gif 목록을 가져옵니다.
+   * @param {string} keyword
+   * @param {number} page
+   * @returns {Promise<GifImageModel[]>}
+   * @ref https://developers.giphy.com/docs/api/endpoint#!/gifs/search
+   */
+  searchByKeyword: async function (keyword: string, page: number): Promise<GifImageModel[]> {
+    const searchOptions: SearchOptions = {
+      limit: DEFAULT_FETCH_COUNT,
+      lang: 'en',
+      offset: page * DEFAULT_FETCH_COUNT
+    };
+
+    try {
+      const gifs: GifsResult = await gf.search(keyword, searchOptions);
+      return convertResponseToModel(gifs.data);
+    } catch (e) {
+      return [];
+    }
+  }
+};
